b9173f1aef88a7610e28acd896e3bec6f7b79a3f
Ed Hynan <freecode@verizon.net>
2013-07-31 09:04:25 -0400

    Updated version to 1.0.1 for branch DEV_1_0_1.

--- version.sh
+++ version.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-V="1.0.0"
+V="1.0.1"
 FMT='%s\n'
 test X"$1" = X-s && FMT='"%s"\n'
-test X"$1" = X-i && V="16777216"
+# 32 bit int: vmajor<<24|vminor<<16|rmajor<<8|rminor
+test X"$1" = X-i && V="16777472"
 printf "$FMT" "$V"
