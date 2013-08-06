47472143da3ed62ad1f85dfbb7031b742f5cb834
Ed Hynan <edhynan@gmail.com>
2013-08-06 19:56:13 -0400

    Various version number updatess for 1.0.2.

--- version.sh
+++ version.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-V="1.0.1"
+V="1.0.2"
 FMT='%s\n'
 test X"$1" = X-s && FMT='"%s"\n'
 # 32 bit int: vmajor<<24|vminor<<16|rmajor<<8|rminor
-test X"$1" = X-i && V="16777472"
+test X"$1" = X-i && V="16777728"
 printf "$FMT" "$V"
