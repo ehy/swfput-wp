a6426a856224ae50d7fa0433dfb0987933a49ddb
Ed Hynan <edhynan@gmail.com>
2013-09-19 08:39:49 -0400

    Bumped revision -- except in readme.txt; if 'Stable tag' is bumped in trunk it looks like new one is released (to WP repo).

--- version.sh
+++ version.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
-V="1.0.3"
+V="1.0.4"
 FMT='%s\n'
 test X"$1" = X-s && FMT='"%s"\n'
 # 32 bit int: vmajor<<24|vminor<<16|rmajor<<8|rminor
-test X"$1" = X-i && V="16777984"
+test X"$1" = X-i && V="16778240"
 printf "$FMT" "$V"
